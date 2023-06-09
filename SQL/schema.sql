3786680de677c1b1e708dce063cbc16b7a4c661c
DiegoCamilli <camillidiego@yahoo.com>
2023-06-09 01:10:09 -0700

    Fixed syntax errors

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,37 +1,30 @@
 
--- set up database
-DROP DATABASE IF EXISTS track_db
-CREATE DATABASE track_db
-USE track_db
+DROP DATABASE IF EXISTS track_db;
+CREATE DATABASE track_db;
+USE track_db;
 
--- create tables with relation keys based on the example on bootcampspot
-CREATE TABLE department (
+-- Create the department table
+CREATE TABLE IF NOT EXISTS department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
-CREATE TABLE role (
+-- Create the role table
+CREATE TABLE IF NOT EXISTS role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
-)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
 
-CREATE TABLE employee (
+-- Create the employee table
+CREATE TABLE IF NOT EXISTS employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT, 
+    role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-)
-
-
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
